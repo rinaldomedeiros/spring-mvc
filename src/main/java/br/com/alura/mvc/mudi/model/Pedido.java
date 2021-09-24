3eee4c7274a61912cb92c6db3c2e2e2cf2ca3e7d
Rinaldo Medeiros <rinaldo.rmedeiros@gmail.com>
2021-09-24 16:15:59 -0300

    Integrando com Spring Data

--- src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
+++ src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
@@ -3,18 +3,37 @@ package br.com.alura.mvc.mudi.model;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
+
+@Entity
 public class Pedido {
 
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Getter	@Setter 
 	private String nomeProduto;
+	
+	@Getter	@Setter 
 	private BigDecimal valorNegociado;
+	
+	@Getter	@Setter 
 	private LocalDate dataDaEntrega;
+	
+	@Getter	@Setter 
 	private String urlProduto;
+	
+	@Getter	@Setter 
 	private String urlImagem;
+	
+	@Getter	@Setter 
 	private String descricao;
 
 }
